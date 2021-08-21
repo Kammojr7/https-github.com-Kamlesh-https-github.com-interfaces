4a7952829d4179e9a83dd8a235c17c8eb611a3cc
Kammojr7 <86921322+Kammojr7@users.noreply.github.com>
2021-08-21 10:59:01 +0530

    Create type_assertion.go

---
+++ type_assertion.go
@@ -0,0 +1,16 @@
+package main
+  
+import "fmt"
+  
+func myfun(a interface{}) {
+  
+    val := a.(string)
+    fmt.Println("Value: ", val)
+}
+func main() {
+  
+    var val interface {
+    } = "KamleshKumar"
+      
+    myfun(val)
+}
